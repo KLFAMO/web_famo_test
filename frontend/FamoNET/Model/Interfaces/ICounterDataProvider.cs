46d25121bb7dd120aea47cbccabda1425eb6e8b5
PiotrekHofi <piotrekhofi@gmail.com>
2025-03-04 19:17:55 +0100

    Cleanup

--- frontend/FamoNET/Model/Interfaces/ICounterDataProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FamoNET.Model.Interfaces
-{
-    public interface ICounterDataProvider
-    {
-        Task<List<DataPoint>> GetData(decimal startMjd, decimal endMjd, string tableName);
-        Task<List<DataPoint>> GetData(string query);
-        Task<List<string>> GetTableNames();
-    }
-}
